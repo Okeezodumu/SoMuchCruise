929eb21c237eb3efd5586b3e68eebb3dfb97b992
unknown <Okeez Odumu>
2024-07-03 15:19:13 +0100

    newest additions

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
-import Home from './Pages/Home'
+import Home from './pages/index';
 import About from './Pages/About'
 import Beatstore from './Pages/Beatstore'
 import Newsletter from './Pages/Newsletter'
